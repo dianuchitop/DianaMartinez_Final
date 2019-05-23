d5760f0657346b05737c89aedde97e6ab08fae0d
dianuchitop <48950463+dianuchitop@users.noreply.github.com>
2019-05-23 11:57:21 -0500

    Update MartinezDiana_final_15.mk

--- MartinezDiana_final_15.mk
+++ MartinezDiana_final_15.mk
@@ -1,5 +1,5 @@
-ode MartinezDiana_codigoc15.cpp:
+ode: MartinezDiana_codigoc15.cpp
     g++ MartinezDiana_codigoc15.cpp
     ./a.out
 graficas: MartinezDiana_codigopython15.py ode
-    python3 artinezDiana_codigopython15.py
+    python3 MartinezDiana_codigopython15.py
